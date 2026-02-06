b57f05261106a019db4692f988b3268686383413
Adarsh Gupta <adarshgupta2888@gmail.com>
2026-02-06 19:41:36 +0530

    Added recursive approach Bubble_sort.java

--- Learn_Important_Sorting_Techniqe/Bubble_sort.java
+++ Learn_Important_Sorting_Techniqe/Bubble_sort.java
@@ -15,4 +15,35 @@ class Solution {
                 break;
         }
     }
+
+    // A function to implement bubble sort
+    static void bubbleSort(int arr[], int n)
+    {
+        // Base case
+        if (n == 1)
+            return;
+
+         int count = 0;
+        // One pass of bubble sort. After
+        // this pass, the largest element
+        // is moved (or bubbled) to end.
+        for (int i=0; i<n-1; i++)
+            if (arr[i] > arr[i+1])
+            {
+                // swap arr[i], arr[i+1]
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                  count = count+1;
+            }
+
+          //check if any swapping occur or not 
+         if (count == 0)
+            return;
+
+        // Largest element is fixed,
+        // recur for remaining array
+        bubbleSort(arr, n-1);
+    }
+
 }
